ff17a45d59b1e62717b50ee19102cc27251816f1
pokerfacer270 <Swidjaja270@gmail.com>
2022-04-16 21:38:18 -0700

    Fixed merge on my branch

--- src/backend/API/src/routes/index.ts
+++ src/backend/API/src/routes/index.ts
@@ -1,23 +1,15 @@
 import express from 'express';
 
 import hotelRoutes from './hotelRoutes';
-<<<<<<< HEAD
 import accountRoutes from './accountRoutes';
-=======
-// import accountRoutes from './accountRoutes';
 import reservationRoutes from './reservationRoutes';
 import paymentRoutes from './paymentRoutes';
->>>>>>> origin/main
 
 const router = express.Router();
 
 router.use('/hotel', hotelRoutes);
-<<<<<<< HEAD
 router.use('/account', accountRoutes);
-=======
-// router.use('/account', accountRoutes);
 router.use('/reservation', reservationRoutes);
 router.use('/payment', paymentRoutes);
->>>>>>> origin/main
 
 export default router;
