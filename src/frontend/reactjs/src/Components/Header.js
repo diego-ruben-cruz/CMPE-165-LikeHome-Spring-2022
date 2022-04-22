294c853c07a4ac2d4a309262435a637b8b496eea
Eric Nguyen <eric.nguyen02@sjsu.edu>
2022-04-22 15:06:30 -0700

    Rebase nando

--- src/frontend/reactjs/src/Components/Header.js
+++ src/frontend/reactjs/src/Components/Header.js
@@ -1,3 +1,41 @@
+// import './Header.css';
+// import React, { Component } from "react";
+// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+// import LoginModal from '../Authentication/LoginModal';
+// import { NavigationState } from "../NavigationContext";
+// import { auth } from '../firebase';
+// import SideDrawer from '../MUI components/SideDrawer';
+// import {Typography, AppBar, CssBaseline, Toolbar, Tabs, Box, Tab, Button} from '@material-ui/core'
+// import { withTheme } from '@emotion/react';
+
+// const Header = () => {
+
+//     const { user} = NavigationState();
+//     // const classes = useStyles();
+//     return (
+//         <>
+//         <br/>
+//         <br/>
+//         <CssBaseline/>
+//             <AppBar style={{ background: 'linear-gradient(#1b59b8, #2B6FD4)'}}>
+//                 <Toolbar>
+//                     <Typography variant="h5">LikeHome</Typography>
+//                     <Tabs style={{marginLeft: 'auto'}} color = 'white'>
+//                         <Tab label="Search"/>
+//                         <Tab label="About us"/>
+//                         <Button style={{borderRadius: '50px'}}variant="contained">Log in</Button>
+//                         <Button style={{marginLeft: '10px', borderRadius: '50px'}} variant="contained">Sign Up</Button>
+//                     </Tabs>
+//                 </Toolbar>
+//             </AppBar>
+//         </>
+//     );
+// }
+
+// export default Header;
+
+// {/* <li>{user ? <SideDrawer/> : <LoginModal/> }</li> */}
+
 import './Header.css';
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
@@ -5,33 +43,30 @@ import LoginModal from '../Authentication/LoginModal';
 import { NavigationState } from "../NavigationContext";
 import { auth } from '../firebase';
 import SideDrawer from '../MUI components/SideDrawer';
-import {Typography, AppBar, CssBaseline, Toolbar, Tabs, Box, Tab, Button} from '@material-ui/core'
-import { withTheme } from '@emotion/react';
 
-const Header = () => {
+function Header() {
 
     const { user} = NavigationState();
-    // const classes = useStyles();
+    
+
     return (
         <>
-        <br/>
-        <br/>
-        <CssBaseline/>
-            <AppBar style={{ background: 'linear-gradient(#1b59b8, #2B6FD4)'}}>
-                <Toolbar>
-                    <Typography variant="h5">LikeHome</Typography>
-                    <Tabs style={{marginLeft: 'auto'}} color = 'white'>
-                        <Tab label="Search"/>
-                        <Tab label="About us"/>
-                        <Button style={{borderRadius: '50px'}}variant="contained">Log in</Button>
-                        <Button style={{marginLeft: '10px', borderRadius: '50px'}} variant="contained">Sign Up</Button>
-                    </Tabs>
-                </Toolbar>
-            </AppBar>
+        <nav className="navHeader">
+            <label className = "label">LikeHome</label>
+            {/* <label className = "label"><Link to="/">LikeHome</Link></label> */}
+            {/* Above line is being worked on to have LikeHome title link to home page */}
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><a href="#">About Us</a></li>
+                <li>{user ? <SideDrawer/> : <LoginModal/> }</li>
+              
+                
+            </ul>
+            
+        </nav>
+        
         </>
     );
 }
 
 export default Header;
-
-// {/* <li>{user ? <SideDrawer/> : <LoginModal/> }</li> */}
--- src/frontend/reactjs/src/Components/Payment.js
+++ src/frontend/reactjs/src/Components/Payment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState } from 'react';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
@@ -9,8 +9,55 @@ import Container from './Container';
 import Card from '@mui/material/Card';
 import Footer from './Footer';
 import Header from './Header';
+import { auth, db } from '../firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import { signInWithEmailAndPassword, updateEmail, updateProfile } from 'firebase/auth';
+import * as api from '../api';
 
-const Payment = () => {
+
+const Payment = ({handleClose}) => {
+
+  const [email, setEmail] = useState("");
+  const [ccv, setCcv] = useState("");
+  const [country, setCountry] = useState("");
+  const [name, setFullName] = useState("");
+  const [expiration, setExp] = useState("");
+  const [number, setNumber] = useState(""); //credit card number
+  
+  
+  //const [name, setName] = useState("");
+  
+
+  const handleSubmit = async () => {
+    try {
+      const paymentResp = await api.payment.pay({
+
+        //Dummy for now
+        accoundId: email,
+        reservationId: '08592959-0906-4086-a50c-05dc3ac5c8ec',
+        paymentDetails: {
+            ccv: ccv,
+            exp: expiration,
+            number: number,
+            country: country,
+            fullname: name,
+            
+
+           },
+        saveDetails: true
+        
+      })
+      console.log(paymentResp);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+
+
   return (
       <>
       <Header/>
@@ -39,6 +86,8 @@ const Payment = () => {
                   label="First name *"
                   variant="outlined"
                   name={'fullName'}
+                  value = {name}
+                  onChange={(e) => setFullName(e.target.value)}
                   fullWidth
                 />
               </Grid>
@@ -54,6 +103,8 @@ const Payment = () => {
                   label="Email *"
                   variant="outlined"
                   name={'email'}
+                  value = {email}
+                  onChange={(e) => setEmail(e.target.value)}
                   fullWidth
                 />
               </Grid>
@@ -69,6 +120,8 @@ const Payment = () => {
                   label="Country *"
                   variant="outlined"
                   name={'country'}
+                  value ={country}
+                  onChange ={(e) => setCountry(e.target.value)}
                   fullWidth
                 />
               </Grid>
@@ -118,6 +171,8 @@ const Payment = () => {
                     label="Card number *"
                     variant="outlined"
                     name={'cardNumber'}
+                    value = {number}
+                    onChange={(e) => setNumber(e.target.value)}
                     fullWidth
                   />
                 </Grid>
@@ -148,6 +203,8 @@ const Payment = () => {
                     label="Expiration date *"
                     variant="outlined"
                     name={'date'}
+                    value = {expiration}
+                    onChange={(e) => setExp(e.target.value)}
                     fullWidth
                   />
                 </Grid>
@@ -178,6 +235,8 @@ const Payment = () => {
                     label="Card CVV *"
                     variant="outlined"
                     name={'cvv'}
+                    value = {ccv}
+                  onChange={(e) => setCcv(e.target.value)}
                     fullWidth
                   />
                 </Grid>
@@ -195,8 +254,9 @@ const Payment = () => {
                   >
                     <Button
                       size={'large'}
-                      variant={'contained'}
+                      variant="contained"
                       type={'submit'}
+                      onClick = {handleSubmit}
                     >
                       Submit
                     </Button>
