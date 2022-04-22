ed9d225c0e0de588e629ec9285721d9866b0fa06
Eric Nguyen <eric.nguyen02@sjsu.edu>
2022-04-22 13:36:43 -0700

    Update Header.js

--- src/frontend/reactjs/src/Components/Header.js
+++ src/frontend/reactjs/src/Components/Header.js
@@ -14,22 +14,24 @@ const Header = () => {
     // const classes = useStyles();
     return (
         <>
-        <nav className="navHeader">
-            <label className = "label">LikeHome</label>
-            {/* <label className = "label"><Link to="/">LikeHome</Link></label> */}
-            {/* Above line is being worked on to have LikeHome title link to home page */}
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><a href="#">About Us</a></li>
-                <li>{user ? <SideDrawer/> : <LoginModal/> }</li>
-              
-                
-            </ul>
-            
-        </nav>
-        
+        <br/>
+        <br/>
+        <CssBaseline/>
+            <AppBar style={{ background: 'linear-gradient(#1b59b8, #2B6FD4)'}}>
+                <Toolbar>
+                    <Typography variant="h5">LikeHome</Typography>
+                    <Tabs style={{marginLeft: 'auto'}} color = 'white'>
+                        <Tab label="Search"/>
+                        <Tab label="About us"/>
+                        <Button style={{borderRadius: '50px'}}variant="contained">Log in</Button>
+                        <Button style={{marginLeft: '10px', borderRadius: '50px'}} variant="contained">Sign Up</Button>
+                    </Tabs>
+                </Toolbar>
+            </AppBar>
         </>
     );
 }
 
 export default Header;
+
+// {/* <li>{user ? <SideDrawer/> : <LoginModal/> }</li> */}
